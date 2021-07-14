d5507f838196c28d6fa9508235e6650b68dcdab3
mohammadaburob <ma@spinup.digital>
2021-07-14 22:54:00 +0300

    organize payment cartridge

--- cartridges/app_custom_payment/cartridge/client/default/js/checkout.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable  no-undef */
-'use strict';
-
-var processInclude = require('base/util');
-
-$(document).ready(function () {
-    processInclude(require('./checkout/checkout'));
-});
