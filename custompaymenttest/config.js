44d68704b5e24559242c2ae9e6f3a900a3db80ca
mohammadaburob <ma@spinup.digital>
2021-08-29 17:34:00 +0300

    add command for test folder

---
+++ custompaymenttest/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    baseUrl: 'https://' + global.baseUrl + '/on/demandware.store/Sites-RefArch-Site/en_US',
+
+    storefrontAuth: {
+        user: 'storefront',
+        pass: 'jifity_payment'
+    }
+};
--- test/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    baseUrl: 'https://zyaf-001.sandbox.us01.dx.commercecloud.salesforce.com/on/demandware.store/Sites-RefArch-Site/en_US',
-
-    storefrontAuth: {
-        user: 'storefront',
-        pass: 'jifity_payment'
-    }
-};
