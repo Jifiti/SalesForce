2b38879c75e8b766c3eefecb29c46467a5df0d42
Dalia Aqraa <dalia.aqraa@fabric.inc>
2022-01-05 11:24:50 +0200

    Fix cart issues

---
+++ cartridges/int_custompayment_sfra/cartridge/client/default/js/messaging/certUtils.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+    initCertMessage: function (id, price, sourcepage) {
+        if (!id || !price) {
+            return;
+        }
+
+        var config = {
+            price: price,
+            link_href: 'https://bjs.citizensone.com/?continueShopping=true',
+            sourcepage: sourcepage || 'product'
+        };
+
+        if ($('#certMessagingText').attr('value')) {
+            config.text = $('#certMessagingText').attr('value');
+        } else if ($('#certMessagingLogoURL').attr('value')) {
+            config.templateName = 'minFinancingWithLogo';
+            config.logo = $('#certMessagingLogoURL').attr('value');
+        } else {
+            config.templateName = 'minFinancing';
+        }
+
+        window.OfferByPrice.init(id, config);
+
+        if ($('#certMessagingTextStyle').attr('value') !== '{}') {
+            window.OfferByPrice.setTextStyle(id, JSON.parse($('#certMessagingTextStyle').attr('value')));
+        }
+
+        if ($('#certMessagingLinkStyle').attr('value') !== '{}') {
+            window.OfferByPrice.setLinkStyle(id, JSON.parse($('#certMessagingLinkStyle').attr('value')));
+        }
+
+        if ($('#certMessagingLinkText').attr('value')) {
+            window.OfferByPrice.setLinkText(id, $('#certMessagingLinkText').attr('value'));
+        }
+
+        if ($('#certMessagingLinkBehaviour').attr('value')) {
+            window.OfferByPrice.setLinkBehavior(id, Number($('#certMessagingLinkBehaviour').attr('value')));
+        }
+
+        $('#' + id + ' .offer-text p').css('color', '');
+    },
+    refreshCertMessage: function () {
+        $('body').on('product:afterAttributeSelect', function (e, data) {
+            if (data && data.container && data.data && data.data.product) {
+                var product = data.data.product;
+                var $productContainer = data.container;
+                var unitPrice = product.price;
+                if (unitPrice.min) {
+                    unitPrice = unitPrice.min;
+                }
+
+                var price = product.selectedQuantity * unitPrice.sales.value;
+                var elem = $productContainer.find('.mount-jifiti-here');
+
+                module.exports.initCertMessage(elem.attr('id'), price, 'product');
+            }
+        });
+    }
+};
--- cartridges/int_custompayment_sfra/cartridge/client/default/js/messaging/messagingPDP.js
+++ cartridges/int_custompayment_sfra/cartridge/client/default/js/messaging/messagingPDP.js
@@ -1,34 +1,21 @@
 'use strict';
 
-/**
- * attaches jifit to the id passed
- * @param {string} id the id to attach jifiti on
- * @param {number} price price
- */
-function mountJifiti(id, price) {
-    if (!id || !price) {
-        return;
-    }
+var certUtils = require('./certUtils');
 
-    window.OfferByPrice.init(id, {
-        price: price,
-        templateName: 'minFinancing',
-        flow: 'initiateFlow',
-        sourcepage: 'product'
-    });
-}
+module.exports = {
+    init: function () {
+        try {
+            window.OfferByPrice.initKey($('#jifitiMerchantId').attr('value'));
 
-module.exports.init = function () {
-    try {
-        window.OfferByPrice.initKey($('#jifitiMerchantId').attr('value'));
+            $('.product-set-detail .product-detail.set-item, .product-bundle-detail, .product-detail.product-wrapper').each(function () {
+                var price = parseFloat($(this).find('.sales .value').attr('content'));
+                var elem = $(this).find('.mount-jifiti-here');
 
-        $('.product-set-detail .product-detail.set-item, .product-bundle-detail, .product-detail.product-wrapper').each(function () {
-            var price = parseFloat($(this).find('.sales .value').attr('content'));
-            var elem = $(this).find('.mount-jifiti-here');
-
-            mountJifiti(elem.attr('id'), price);
-        });
-    } catch (e) {
-        // console.log("error", e)
-    }
+                certUtils.initCertMessage(elem.attr('id'), price, 'product');
+            });
+        } catch (e) {
+            // console.log("error", e)
+        }
+    },
+    refreshCertMessage: certUtils.refreshCertMessage
 };
