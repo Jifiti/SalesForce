bbe0e80b623e3b59310986fb5f0ef34392595feb
Leen Odeh <lodeh@itgsoftware.com>
2023-01-10 12:30:49 +0200

    add capture helper

--- cartridges/int_custompayment_sfra/cartridge/scripts/checkout/credPaymentHelper.js
+++ cartridges/int_custompayment_sfra/cartridge/scripts/checkout/credPaymentHelper.js
@@ -73,24 +73,21 @@ function handlePayments(order, orderNumber, orderToken) {
     return result;
 }
 
-function getCorrectURL(url, orderId) {
+function cancelOrder(orderId) {
+    var credCancelService = require('*/cartridge/scripts/checkout/svc/orderApi');
+    var svc = credCancelService.sendOrderRequest();
+    var url = Site.current.getCustomPreferenceValue('jifitiCancelApi');
     var order = OrderMgr.getOrder(orderId);
     var paymentInstruments = order.paymentInstruments;
     var authId;
+    var isPartialCommited = false;
     for (var i = 0; i < paymentInstruments.length; i++) {
         if (paymentInstruments[i].paymentMethod === 'CRED_PAYMENT') {
-            authId = paymentInstruments[i].paymentTransaction.getTransactionID();
+            authId = order.custom.RemainderAuthId ? order.custom.RemainderAuthId : paymentInstruments[i].paymentTransaction.getTransactionID();
+            isPartialCommited = !empty(order.custom.RemainderAuthId);
         }
     }
     url = url.replace('AuthId',authId);
-    return url;
-}
-
-function cancelOrder(orderId) {
-    var credCancelService = require('*/cartridge/scripts/checkout/svc/cancelOrRefundOrderApi');
-    var svc = credCancelService.sendCancelOrRefundRequest();
-    var url = Site.current.getCustomPreferenceValue('jifitiCancelApi');
-    url = getCorrectURL(url, orderId);
     var body = {};
     body.OrderId = orderId;
     var params = {};
@@ -98,17 +95,19 @@ function cancelOrder(orderId) {
     params.URL = url;
     var result = svc.call(params);
     if (result.ok) {
-        return JSON.parse(result.object.text);
+        var response = JSON.parse(result.object.text);
+        response.isPartialCommited = isPartialCommited;
+        return response;
     } else {
         return result.errorMessage;
     }
 }
 
-function refundOrder(orderId, requestedAmount, currency) {
-    var credRefundService = require('*/cartridge/scripts/checkout/svc/cancelOrRefundOrderApi');
-    var svc = credRefundService.sendCancelOrRefundRequest();
+function callRefund(authId, orderId, requestedAmount, currency) {
+    var credRefundService = require('*/cartridge/scripts/checkout/svc/orderApi');
+    var svc = credRefundService.sendOrderRequest();
     var url = Site.current.getCustomPreferenceValue('jifitiRefundApi');
-    url = getCorrectURL(url, orderId);
+    url = url.replace('AuthId', authId);
     var body = {};
     body.OrderId = orderId;
     body.RequestedAmount = requestedAmount;
@@ -118,8 +117,101 @@ function refundOrder(orderId, requestedAmount, currency) {
     params.body = body;
     params.URL = url;
     var result = svc.call(params);
+    return result;
+}
+
+function refundOrder(orderId, requestedAmount, currency) {
+    var Transaction = require('dw/system/Transaction');
+    var order = OrderMgr.getOrder(orderId);
+    var paymentInstruments = order.paymentInstruments;
+    var authId, commitedArr, commitedStr, totalCommited = 0, result, refundedAmount, commitedJSON, updateCommitedStr = '';
+    for (var i = 0; i < paymentInstruments.length; i++) {
+        if (paymentInstruments[i].paymentMethod === 'CRED_PAYMENT') {
+            if (order.custom.CommitedAuthId) {
+                commitedStr = order.custom.CommitedAuthId;
+                commitedArr = commitedStr.split('||');
+                for (var j = 0; j < commitedArr.length; j++) {
+                    totalCommited += JSON.parse(commitedArr[j]).CommitedAmount;
+                }
+                if (totalCommited >= requestedAmount) {
+                    for (var j = 0; j < commitedArr.length && requestedAmount > 0 ; j++) {
+                        commitedJSON = JSON.parse(commitedArr[j]);
+                        refundedAmount = requestedAmount >= commitedJSON.CommitedAmount ? commitedJSON.CommitedAmount : requestedAmount;
+                        if (refundedAmount > 0 && commitedJSON.CommitedAmount > 0) {
+                            result = callRefund(commitedJSON.CommitedAuthId, orderId, refundedAmount, currency);
+                            if (result.ok) {
+                                    requestedAmount -= refundedAmount;
+                                    commitedJSON.CommitedAmount = commitedJSON.CommitedAmount - refundedAmount;
+                                    commitedJSON = JSON.stringify(commitedJSON);
+                                    commitedArr[j] = commitedJSON;
+                            } else {
+                                return result.errorMessage;
+                            }
+                        }
+                    }
+                    for (var j = 0; j < commitedArr.length; j++) {
+                        updateCommitedStr += commitedArr[j] + '||';
+                    }
+                    updateCommitedStr = updateCommitedStr.substring(0, updateCommitedStr.lastIndexOf('||'));
+                    Transaction.wrap(function () {
+                        order.custom.CommitedAuthId = updateCommitedStr;
+                        updateCommitedStr = '';
+                    });
+                    return result;
+                } else {
+                    var response = {};
+                    response.errorMessage = 'Requested amount is bigger than commited amount';
+                    response.requestedAmount = requestedAmount;
+                    response.CommitedAmount = totalCommited;
+                    return response;
+                }
+            } else {
+                authId = paymentInstruments[i].paymentTransaction.getTransactionID();
+                result = callRefund(authId, orderId, requestedAmount, currency);
+                if (result.ok) {
+                    return result;
+                } else {
+                    return result.errorMessage;
+                }
+            }
+        }
+    }
+}
+
+function captureOrder(orderId, commitAmount){
+    var Transaction = require('dw/system/Transaction');
+    var Order = require('dw/order/Order');
+    var credCaptureService = require('*/cartridge/scripts/checkout/svc/orderApi');
+    var svc = credCaptureService.sendOrderRequest();
+    var url = Site.current.getCustomPreferenceValue('jifitiCaptureApi');
+    var order = OrderMgr.getOrder(orderId);
+    var paymentInstruments = order.paymentInstruments;
+    var authId;
+    for (var i = 0; i < paymentInstruments.length; i++) {
+        if (paymentInstruments[i].paymentMethod === 'CRED_PAYMENT') {
+            authId = order.custom.RemainderAuthId ? order.custom.RemainderAuthId : paymentInstruments[i].paymentTransaction.getTransactionID();
+        }
+    }
+    url = url.replace('AuthId',authId);
+    var body = {};
+    body.OrderId = orderId;
+    body.CommitAmount = commitAmount;
+    var params = {};
+    params.body = body;
+    params.URL = url;
+    var result = svc.call(params);
     if (result.ok) {
-        return JSON.parse(result.object.text);
+        var response = JSON.parse(result.object.text);
+        Transaction.wrap(function () {
+            order.custom.CommitedAuthId = order.custom.CommitedAuthId ? order.custom.CommitedAuthId + '||' + '{"CommitedAuthId":"'+ response.CommitedAuthId + '", "CommitedAmount":' + commitAmount + '}' : '{"CommitedAuthId":"'+ response.CommitedAuthId + '", "CommitedAmount":' + commitAmount + '}';
+            order.custom.RemainderAuthId = response.RemainderAuthId;
+            if(response.RemainderAmount > 0) {
+                order.setPaymentStatus(Order.PAYMENT_STATUS_PARTPAID);
+            } else {
+                order.setPaymentStatus(Order.PAYMENT_STATUS_PAID);
+            }
+        });
+        return response;
     } else {
         return result.errorMessage;
     }
@@ -128,5 +220,6 @@ function refundOrder(orderId, requestedAmount, currency) {
 module.exports = exports = {
     handlePayments: handlePayments,
     cancelOrder: cancelOrder,
-    refundOrder: refundOrder
+    refundOrder: refundOrder,
+    captureOrder: captureOrder
 };
